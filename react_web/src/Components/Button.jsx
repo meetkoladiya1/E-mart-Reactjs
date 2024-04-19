c74fcbfb6e155721d0e171dcc3c3af44971c864b
whoisdarshan <darshansavaliya2005@gmail.com>
2024-04-19 17:15:02 +0530

    19-04-2024

---
+++ react_web/src/Components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='btn bg-purple-500 m-4'>click me</button>
+    </div>
+  )
+}
+
+export default Button
